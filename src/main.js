638e196cc8aec9f84012b15ef9acd7851ce38c1e
泡海椒 <814780613@qq.com>
2020-02-17 10:20:07 +0800

    改造文件输入

--- src/main.js
+++ src/main.js
@@ -22,6 +22,26 @@ Vue.use(Toast, 3000)
 Vue.use(Loading)
 import Pagination from 'vue-pagination-2'
 Vue.component('pagination', Pagination)
+Vue.filter('formatDate', function(datestr) {
+  if (!datestr) {
+    return ''
+  } else {
+    let timestamp = new Date(datestr).getTime()
+    let date = new Date(timestamp)
+    let YY = date.getFullYear() + '-'
+    let MM =
+      (date.getMonth() + 1 < 10
+        ? '0' + (date.getMonth() + 1)
+        : date.getMonth() + 1) + '-'
+    let DD = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+    let hh =
+      (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':'
+    let mm =
+      (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':'
+    let ss = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+    return YY + MM + DD + ' ' + hh + mm + ss
+  }
+})
 new Vue({
   el: '#app',
   router,
