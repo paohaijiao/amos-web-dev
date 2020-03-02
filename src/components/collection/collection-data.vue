26506732e8f062d81b788b8b3057413e90768b64
泡海椒 <814780613@qq.com>
2020-03-02 19:00:07 +0800

    修改提示语

--- src/components/collection/collection-data.vue
+++ src/components/collection/collection-data.vue
@@ -53,7 +53,7 @@
                     <div class="modal-content">
                       <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title" id="myModalLabel">数据采集</h4>
+                        <h4 class="modal-title" id="myModalLabel">数据采集(仅支持原生spark支持的类型)</h4>
                       </div>
                       <div class="box-body">
                         <div class="form-group">
