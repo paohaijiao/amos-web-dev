0f3f3fecd2da112116a1102ce97b223be1f068a3
goudingcheng <814780613@qq.com>
2020-03-20 09:50:34 +0800

    文本文件输出

--- src/components/topo/dialog/table-textfileoutput.vue
+++ src/components/topo/dialog/table-textfileoutput.vue
@@ -32,7 +32,7 @@
         </tr>
         </thead>
         <tbody>
-        <tr v-for="item in tableData">
+        <tr v-for="(item,index) in tableData">
           <td>
             <input type="text" class="form-control" v-model="item.field_name">
           </td>
@@ -42,7 +42,7 @@
             </select>
           </td>
           <td>
-            <button type="button" class="btn btn-info" @click="handleDelete($index, item)">删除</button>
+            <button type="button" class="btn btn-info" @click="handleDelete(index, item)">删除</button>
           </td>
 
         </tr>
