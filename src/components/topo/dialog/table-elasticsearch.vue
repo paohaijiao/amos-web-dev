67bfbcb69403cb2233898a10ac7656a2c399e8d3
泡海椒 <814780613@qq.com>
2020-04-11 20:01:33 +0800

    批处理

--- src/components/topo/dialog/table-elasticsearch.vue
+++ src/components/topo/dialog/table-elasticsearch.vue
@@ -14,7 +14,7 @@
     </div>
     <div class="form-group">
       <label for="name" >端口号 </label>
-      <input type="text" class="form-control"   v-model="form.server_port" placeholder="请输入端口号">
+      <input type="number" class="form-control"   v-model="form.server_port" placeholder="请输入端口号">
     </div>
     <div class="form-group">
       <label for="name" >参数名 </label>
@@ -36,6 +36,34 @@
     <div class="form-group">
       <label for="name" >主键</label>
       <input type="text" class="form-control"   v-model="form.general_idField" placeholder="请输入主键">
+    </div>
+    <div>
+      <div class="form-group">
+        <label  >线程数(1或更多)</label>
+        <input type="number" class="form-control" min="1"  v-model="form.copy" placeholder="请输入需要使用的线程数"></input>
+      </div>
+      <div class="form-group">
+        <label>执行集群<span style="color:red">(可选)</span></label>
+        <select  v-model="form.cluster_schema" class="form-control select2 select2-hidden-accessible">
+          <option v-for="item in clusters"  :key="item.name" :label="item.name" :value="item.name"></option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label>分区方式<span style="color:red">(可选)</span></label>
+        <select  v-model="form.PARTITIONING_METHOD" class="form-control select2 select2-hidden-accessible">
+          <option v-for="item in partionType"  :key="item.key" :label="item.value" :value="item.key"></option>
+        </select>
+      </div>
+      <div class="form-group" v-if="form.PARTITIONING_METHOD=='Mirror'">
+        <label>分区节点<span style="color:red">(可选)</span></label>
+        <select  v-model="form.PARTITIONING_SCHEMA" class="form-control select2 select2-hidden-accessible">
+          <option v-for="item in partionNode"  :key="item.name" :label="item.name" :value="item.name"></option>
+        </select>
+      </div>
+      <div class="form-group" v-if="form.PARTITIONING_METHOD=='ModPartitioner'">
+        <label>分区字段<span style="color:red">(可选)</span></label>
+        <input type="text" class="form-control" min="1"  v-model="form.PARTITIONING_SCHEMA" placeholder="请输入分区字段"></input>
+      </div>
     </div>
     <div class="box">
       <div class="box-header with-border">
@@ -83,7 +111,10 @@ export default {
       form: _.cloneDeep(this.item.data) || {},
       dialogVisible: true,
       typeoptions: [],
-      tableData: []
+      tableData: [],
+      clusters:[],
+      partionType:[],
+      partionNode:[]
     }
   },
   methods: {
@@ -92,6 +123,25 @@ export default {
       this.item.data = this.form
       $('#myModal').modal('hide')
       this.onClose();
+    },
+    initCluster() {
+      let param=new Object();
+      this.$api.getClusterNotPage(param,res => {
+        if (res.code === 200) {
+          this.clusters = res.data
+        }
+      })
+      this.$api.getPartionType(param,res => {
+        if (res.code === 200) {
+          this.partionType = res.data
+        }
+      })
+      this.$api.getPartitionNode(param,res => {
+        if (res.code === 200) {
+          this.partionNode= res.data
+        }
+      })
+
     },
     getField(){
           let param=new Object();
@@ -147,6 +197,7 @@ export default {
   },
   created() {
     this.getSource()
+    this.initCluster()
     this.tableData = this.form.field ? this.form.field : []
   }
 }
--- src/components/topo/dialog/table-pg-load.vue
+++ src/components/topo/dialog/table-pg-load.vue
@@ -30,6 +30,34 @@
       <label  >分隔符</label>
       <input type="text" class="form-control"   v-model="form.delimiter" placeholder="请输入分隔符"></input>
     </div>
+    <div>
+      <div class="form-group">
+        <label  >线程数(1或更多)</label>
+        <input type="number" class="form-control" min="1"  v-model="form.copy" placeholder="请输入需要使用的线程数"></input>
+      </div>
+      <div class="form-group">
+        <label>执行集群<span style="color:red">(可选)</span></label>
+        <select  v-model="form.cluster_schema" class="form-control select2 select2-hidden-accessible">
+          <option v-for="item in clusters"  :key="item.name" :label="item.name" :value="item.name"></option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label>分区方式<span style="color:red">(可选)</span></label>
+        <select  v-model="form.PARTITIONING_METHOD" class="form-control select2 select2-hidden-accessible">
+          <option v-for="item in partionType"  :key="item.key" :label="item.value" :value="item.key"></option>
+        </select>
+      </div>
+      <div class="form-group" v-if="form.PARTITIONING_METHOD=='Mirror'">
+        <label>分区节点<span style="color:red">(可选)</span></label>
+        <select  v-model="form.PARTITIONING_SCHEMA" class="form-control select2 select2-hidden-accessible">
+          <option v-for="item in partionNode"  :key="item.name" :label="item.name" :value="item.name"></option>
+        </select>
+      </div>
+      <div class="form-group" v-if="form.PARTITIONING_METHOD=='ModPartitioner'">
+        <label>分区字段<span style="color:red">(可选)</span></label>
+        <input type="text" class="form-control" min="1"  v-model="form.PARTITIONING_SCHEMA" placeholder="请输入分区字段"></input>
+      </div>
+    </div>
     <div class="box">
       <div class="box-header with-border">
         <label  style="line-height: 35px;">字段</label>
@@ -83,7 +111,10 @@
                 dialogVisible: true,
                 options: [],
                 tableData: [],
-                option:[]
+                option:[],
+              clusters:[],
+              partionType:[],
+              partionNode:[]
             }
         },
         methods: {
@@ -96,6 +127,25 @@
           cancel() {
             $('#myModal').modal('hide')
             this.onClose()
+          },
+          initCluster() {
+            let param=new Object();
+            this.$api.getClusterNotPage(param,res => {
+              if (res.code === 200) {
+                this.clusters = res.data
+              }
+            })
+            this.$api.getPartionType(param,res => {
+              if (res.code === 200) {
+                this.partionType = res.data
+              }
+            })
+            this.$api.getPartitionNode(param,res => {
+              if (res.code === 200) {
+                this.partionNode= res.data
+              }
+            })
+
           },
             onClose() {
                 this.dialogVisible = false
@@ -140,6 +190,7 @@
             }
         },
         created() {
+            this.initCluster();
             var obj=new Object();
             obj.id=1;
             obj.name='PASS THROUGH';
