5316894c9d8e755684116e2fb54716b6d7de7235
泡海椒 <814780613@qq.com>
2020-04-11 15:23:50 +0800

    流程节点

--- src/components/topo/dialog/table-ifnull.vue
+++ src/components/topo/dialog/table-ifnull.vue
@@ -8,6 +8,34 @@
       <label for="name" >步骤名称</label>
       <input type="text" class="form-control" id="name"   v-model="form.name" placeholder="请输入步骤名称">
     </div>
+    <div>
+      <div class="form-group">
+        <label  >线程数(1或更多)</label>
+        <input type="number" class="form-control" min="1"  v-model="form.copy" placeholder="请输入需要使用的线程数"></input>
+      </div>
+      <div class="form-group">
+        <label>执行集群<span style="color:red">(可选)</span></label>
+        <select  v-model="form.cluster_schema" class="form-control select2 select2-hidden-accessible">
+          <option v-for="item in clusters"  :key="item.name" :label="item.name" :value="item.name"></option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label>分区方式<span style="color:red">(可选)</span></label>
+        <select  v-model="form.PARTITIONING_METHOD" class="form-control select2 select2-hidden-accessible">
+          <option v-for="item in partionType"  :key="item.key" :label="item.value" :value="item.key"></option>
+        </select>
+      </div>
+      <div class="form-group" v-if="form.PARTITIONING_METHOD=='Mirror'">
+        <label>分区节点<span style="color:red">(可选)</span></label>
+        <select  v-model="form.PARTITIONING_SCHEMA" class="form-control select2 select2-hidden-accessible">
+          <option v-for="item in partionNode"  :key="item.name" :label="item.name" :value="item.name"></option>
+        </select>
+      </div>
+      <div class="form-group" v-if="form.PARTITIONING_METHOD=='ModPartitioner'">
+        <label>分区字段<span style="color:red">(可选)</span></label>
+        <input type="text" class="form-control" min="1"  v-model="form.PARTITIONING_SCHEMA" placeholder="请输入分区字段"></input>
+      </div>
+    </div>
     <div class="box">
       <div class="box-header with-border">
         <label style="line-height: 35px;">字段</label>
@@ -72,7 +100,10 @@ export default {
           value: 'N'
         }
       ],
-      tableData: []
+      tableData: [],
+      clusters:[],
+      partionType:[],
+      partionNode:[],
     }
   },
   methods: {
@@ -85,6 +116,25 @@ export default {
     cancel() {
       $('#myModal').modal('hide')
       this.onClose()
+    },
+    initCluster() {
+      let param=new Object();
+      this.$api.getClusterNotPage(param,res => {
+        if (res.code === 200) {
+          this.clusters = res.data
+        }
+      })
+      this.$api.getPartionType(param,res => {
+        if (res.code === 200) {
+          this.partionType = res.data
+        }
+      })
+      this.$api.getPartitionNode(param,res => {
+        if (res.code === 200) {
+          this.partionNode= res.data
+        }
+      })
+
     },
     onClose() {
       this.dialogVisible = false
@@ -124,6 +174,7 @@ export default {
     }
   },
   created() {
+    this.initCluster();
     this.tableData = this.form.field ? this.form.field : []
   }
 
