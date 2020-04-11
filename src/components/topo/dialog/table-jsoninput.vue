43254a8c5819cce280c2dce6d603216241b7ac9d
泡海椒 <814780613@qq.com>
2020-04-11 09:32:07 +0800

    支持json輸入

--- src/components/topo/dialog/table-jsoninput.vue
+++ src/components/topo/dialog/table-jsoninput.vue
@@ -34,6 +34,33 @@
       <label >字段名</label>
       <input type="text" class="form-control"   v-model="form.valueField" placeholder="请输入字段名">
     </div>
+    <div class="form-group">
+      <label  >线程数(1或更多)</label>
+      <input type="number" class="form-control" min="1"  v-model="form.copy" placeholder="请输入需要使用的线程数"></input>
+    </div>
+    <div class="form-group">
+      <label>执行集群<span style="color:red">(可选)</span></label>
+      <select  v-model="form.cluster_schema" class="form-control select2 select2-hidden-accessible">
+        <option v-for="item in clusters"  :key="item.name" :label="item.name" :value="item.name"></option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label>分区方式<span style="color:red">(可选)</span></label>
+      <select  v-model="form.PARTITIONING_METHOD" class="form-control select2 select2-hidden-accessible">
+        <option v-for="item in partionType"  :key="item.key" :label="item.value" :value="item.key"></option>
+      </select>
+    </div>
+    <div class="form-group" v-if="form.PARTITIONING_METHOD=='Mirror'">
+      <label>分区节点<span style="color:red">(可选)</span></label>
+      <select  v-model="form.PARTITIONING_SCHEMA" class="form-control select2 select2-hidden-accessible">
+        <option v-for="item in partionNode"  :key="item.name" :label="item.name" :value="item.name"></option>
+      </select>
+    </div>
+    <div class="form-group" v-if="form.PARTITIONING_METHOD=='ModPartitioner'">
+      <label>分区字段<span style="color:red">(可选)</span></label>
+      <input type="text" class="form-control" min="1"  v-model="form.PARTITIONING_SCHEMA" placeholder="请输入分区字段"></input>
+    </div>
+
 
     <div class="box">
       <div class="box-header with-border">
@@ -90,7 +117,10 @@ export default {
         {name: 'Y', value: 'Y' },
         {name: 'N', value: 'N' }
       ],
-      tableData: []
+      tableData: [],
+      clusters:[],
+      partionType:[],
+      partionNode:[]
     }
   },
   methods: {
@@ -124,6 +154,25 @@ export default {
               }
           })
       },
+    initCluster() {
+      let param=new Object();
+      this.$api.getClusterNotPage(param,res => {
+        if (res.code === 200) {
+          this.clusters = res.data
+        }
+      })
+      this.$api.getPartionType(param,res => {
+        if (res.code === 200) {
+          this.partionType = res.data
+        }
+      })
+      this.$api.getPartitionNode(param,res => {
+        if (res.code === 200) {
+          this.partionNode= res.data
+        }
+      })
+
+    },
     onClose() {
       this.dialogVisible = false
       this.$emit('on-close', this.item)
@@ -155,6 +204,7 @@ export default {
   },
   created() {
     this.getSource()
+    this.initCluster();
     this.tableData = this.form.field ? this.form.field : []
   }
 }
